20c2125ad623e226499b3967c6badbad53b563dc
Sauhard Dobhal <17234141+sauharddobhal@users.noreply.github.com>
2019-08-31 17:19:33 +0530

    Update contact_me.js

--- contact/contact_me.js
+++ contact/contact_me.js
@@ -45,7 +45,7 @@ $(function() {
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that we're facing technical issues...</strong> Could you please email us directly to <a href='mailto:info@vs-infotech.in?Subject= From vs-infotech.in'>info@vs-infotech.in</a> ? Sorry for the inconvenience!");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
